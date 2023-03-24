933adf0b2b3daf990c82c6a62a499c11813a4a4e
saihnii4 <81787757+saihnii4@users.noreply.github.com>
2023-03-24 13:37:02 +0800

    clean up

--- 1.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int A, B;
-    scanf("%d %d", &A, &B);
-    if (A == 2) return printf("2\n");
-    else if (B == 2) return printf("3\n");
-    if (B >= A) return printf("%d\n", 2*(A-1));
-    else return printf("%d\n", 2*B-1);
-}
