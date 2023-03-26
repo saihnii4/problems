5528bd0bcaf6fc9cd39cd642d3d0f0046d699731
saihnii4 <81787757+dulguuncodes@users.noreply.github.com>
2023-03-27 07:45:18 +0800

    bruh

--- cses/subarray_sums.cpp
+++ cses/subarray_sums.cpp
@@ -1,31 +1,35 @@
 #include <stdio.h>
 #include <vector>
+#include <string.h>
 
 using namespace std;
 
-long long n, x, count = 0;
+bool memo[200000];
+long long n, x;
+long long coun = 0;
 
-void generate(long long arr[], long long i, long long sum, bool traversed[]) {
+void generate(long long arr[], long long i, long long sum) {
+    if (i == n - 1) return;
     if (sum == x) {
-        if (traversed[i]) return;
-        traversed[i] = true;
-        ++count;
-        return;
+        ++coun;
     }
-    if (i == n - 1) return; 
-    generate(arr, i+1, sum + arr[i + 1], traversed);
-    generate(arr, i+1, arr[i + 1], traversed);
+
+    if (!memo[i]) {
+        memo[i] = true;
+        generate(arr, i + 1, arr[i + 1]);
+    }
+
+    generate(arr, i + 1, sum+arr[i + 1]);
 }
 
 int main() {
+    memset(memo, false, sizeof(bool)*200000);
+
     scanf("%lld %lld", &n, &x);
-    bool traversed[n];
     long long arr[n];
 
-    for (int i = 0; i < n; i++) scanf("%lld", &arr[i]);
+    for (long long i = 0; i < n; i++) scanf("%lld", &arr[i]);
+    generate(arr, 0, arr[0]);
 
-    generate(arr, 0, arr[0], traversed);
-
-    printf("%lld\n", count);
-    return 0;
+    printf("%lld\n", coun);
 }
