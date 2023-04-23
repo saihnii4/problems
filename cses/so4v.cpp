7f6b9cd3e4247ac089eeb1be63afcf61946d4790
saihnii4 <81787757+dulguuncodes@users.noreply.github.com>
2023-04-23 09:17:05 +0800

    so4v

--- cses/so4v.cpp
+++ cses/so4v.cpp
@@ -1,4 +1,5 @@
 #include <set>
+#include <algorithm>
 #include <stdio.h>
 #include <tuple>
 #include <utility>
@@ -14,18 +15,23 @@ bool check(int a, int b, int c, int d) {
   return a != b && a != c && a != d && b != c && b != d && c != d;
 }
 
+bool cmp(el a, el b) {
+    return get<0>(a) < get<0>(b);
+}
+
 int main() {
   scanf("%d %lld", &n, &k);
   long long arr[n];
 
   for (int i = 0; i < n; i++) {
     scanf("%lld", &arr[i]);
-    if (arr[i] > mx) mx = arr[i];
+    if (arr[i] > mx)
+      mx = arr[i];
   }
 
   if (mx * 4 < k) {
-      printf("IMPOSSIBLE\n");
-      return 0;
+    printf("IMPOSSIBLE\n");
+    return 0;
   }
 
   for (int i = 0; i < n; i++) {
@@ -38,15 +44,17 @@ int main() {
   }
 
   for (el e : s) {
-    for (el j : s)
-      if (e != j && check(get<1>(e).first, get<1>(e).second, get<1>(j).first,
-                          get<1>(j).second)) {
-        if (get<0>(e) + get<0>(j) == k) {
-          printf("%d %d %d %d\n", get<1>(e).first + 1, get<1>(e).second + 1,
-                 get<1>(j).first + 1, get<1>(j).second + 1);
-          return 0;
-        }
-      }
+    auto it = s.lower_bound(el(k - get<0>(e), pair<int, int>(0, 0)));
+
+    if (!check(get<1>(e).first, get<1>(e).second, get<1>(*it).first,
+               get<1>(*it).second) ||
+        get<0>(*it) + get<0>(e) != k)
+      continue;
+    el j = *it;
+
+    printf("%d %d %d %d\n", get<1>(e).first + 1, get<1>(e).second + 1,
+           get<1>(j).first + 1, get<1>(j).second + 1);
+    return 0;
   }
 
   printf("IMPOSSIBLE\n");
