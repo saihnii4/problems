d82751c62afd75ad12c91b36c0a58de8d2d47b4f
saihnii4 <81787757+dulguuncodes@users.noreply.github.com>
2023-10-07 21:38:09 +0800

    removing book problems

---
+++ usaco/subsequences_sevens.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    freopen("div7.in", "r", stdin);
+    freopen("div7.out", "w", stdout);
+    for (int i=0;i<n;i++) {
+
+    }
+}
