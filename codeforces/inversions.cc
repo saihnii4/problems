f23d57e7ececc3615cbf3b859c1b6249a7d7c236
saihnii4 <81787757+dulguuncodes@users.noreply.github.com>
2023-10-07 21:38:03 +0800

    inversion attempts

--- codeforces/inversions.cc
+++ codeforces/inversions.cc
@@ -1,21 +1,56 @@
 #include <iostream>
+#define HEAD_NODE 0
 using namespace std;
 typedef long long ll;
 int n;
 ll seg[400000];
-void build(ll id,ll L, ll R) {
-    if (L==R) {
+void build(ll id, ll L, ll R)
+{
+    if (L == R)
+    {
         seg[id] = 0;
         return;
     }
-    build(2*id+1,L,M);
-    build(2*id+2,M+1,R);
-    seg[id]
+    ll M = (L + R) / 2;
+    build(2 * id + 1, L, M);
+    build(2 * id + 2, M + 1, R);
 }
-int main() {
-    cin >> n;
-    build(HEAD_NODE,0,n-1);
-    for (int i=0;i<n;i++) {
-        cin >> t;
+ll query(ll id, ll L, ll R, ll l, ll r)
+{
+    if (L == l && r == R)
+        return seg[id];
+    ll M = (L + R) / 2;
+    if (l >= M + 1)
+        return query(2 * id + 2, M+ 1, R, l, r);
+    else
+        return query(2 * id + 1, L, M, l, M) + query(2 * id + 2, M + 1, R, M + 1, r);
+}
+void update(ll id, ll L, ll R, ll k)
+{
+    if (L == R)
+    {
+        seg[id] = 1;
+        return;
     }
+
+    ll M = (L + R) / 2;
+    if (k <= M)
+        update(2 * id + 1, L, M, k);
+    else
+        update(2 * id + 2, M + 1, R, k);
+    seg[id]=seg[2*id+1]+seg[2*id+2];
+}
+int main()
+{
+    cin >> n;
+    build(HEAD_NODE, 0, n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        int t;
+        cin >> t;
+        if (t < n) cout << query(HEAD_NODE, 0, n - 1, t-1, n-1) << endl;
+        else cout << 0 << endl;
+        update(HEAD_NODE, 0, n - 1, t);
+    }
+    return 0;
 }
